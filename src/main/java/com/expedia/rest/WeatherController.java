9d0e999b0c1db003aefa14c4a90e5b0b1f0ef1f8
anuraggupta <anurag.11feb@gmail.com>
2013-12-04 14:34:07 +0530

    Revert "added service"
    This reverts commit 198c74f9c00188aa542dcaed799b8db87007543d.

--- src/main/java/com/expedia/rest/WeatherController.java
+++ src/main/java/com/expedia/rest/WeatherController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping( value = "conditions" )
+@RequestMapping( value = "" )
 public class WeatherController {
 
 	@RequestMapping("/zip/{zip}")
--- weather/src/main/java/com/expedia/service/WeatherInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.expedia.service;
-
-import com.expedia.rest.Weather;
-
-public interface WeatherInfoService {
-
-	public Weather getConditionsByZip(Integer zip);
-	
-}
